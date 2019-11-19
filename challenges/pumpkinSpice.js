e6ee43136564d26aed12eb53f4b02c5227b4a3b6
Trisha Aguinaldo <trisha.aguinaldo@gmail.com>
2019-11-19 05:06:55 -0500

    setup for day19 challenge

---
+++ challenges/pumpkinSpice.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const pie = {
+  cost: 5,
+  spice: 30
+};
+  
+const latte = {
+  cost: 3,
+  spice: 15
+};
+  
+const macaron = {
+  cost: 1,
+  spice: 3
+};
+
+const input = 9;
+const output = [1, 1, 1, 48];
+
+const pumpkinSpice = money => {
+  let result = [0,0,0,0];
+  // while money >= 0 then calculate how much it can buy pie, then lattes, then macaron
+  
+  //   while()
+  return result;
+};
+
+console.log(pumpkinSpice(input));
